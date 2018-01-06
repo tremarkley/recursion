5dc985432eb69a8ca401e3a9a7e605ca72e8923e
Harry Markley <tremarkley@gmail.com>
2018-01-06 15:04:49 -0800

    Wrote getElementsByClassName recursively with all test cases passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,24 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var result = [];
+  if (this.className != undefined){
+    if (this.className.indexOf(className) > -1){
+      result.push(this);
+    }
+  }
+  if (this === window){
+    for (let i = 0; i < this.document.children.length; i++){
+      result = result.concat(getElementsByClassName.call(this.document.children[i], className));
+    }
+  }
+  else {
+    if (this.children != undefined){
+      for (let i = 0; i < this.children.length; i++){
+        result = result.concat(getElementsByClassName.call(this.children[i], className));
+      }
+    }
+  }
+  return result;
 };
+
