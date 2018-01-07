2392469ba786f5d841db21cdd4502b3d8b9a3385
Harry Markley <tremarkley@gmail.com>
2018-01-06 16:41:59 -0800

    Write stringifyJSON from scratch recursively with all test cases passing.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj == 'string'){
+    var strArr = String(obj).split();
+    return "\"" + obj + "\"";
+  }
+  else if (Array.isArray(obj)) {
+    var result = "";
+    for (let i = 0; i < obj.length; i++){
+      if (result != ""){
+        result = result + "," + stringifyJSON(obj[i]);
+      }
+      else {
+        result = stringifyJSON(obj[i]);
+      }
+    }
+    return "[" + result + "]";
+  }
+  else if (obj !== null && typeof obj === 'object') {
+    var result = "";
+    for (var prop in obj){
+      //JSON does not allow you to stringify functions or undefined values
+      if (typeof obj[prop] !== 'function' && typeof obj[prop] !== 'undefined'){
+        if (result != ""){
+          result = result + "," + stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]);
+        }
+        else {
+          result = stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]);
+        }
+      }
+    }
+    return "{" + result + "}";
+  }
+  else {
+    return String(obj);
+  }
 };
